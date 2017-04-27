b7f205bef8fd09e3a1d3e0fa69dd81f1f2b78dba
dlewin <dlewin@free.fr>
2017-04-27 19:39:01 +0200

    Big update
    SavePattern: Defined a complete header
    Added LoadPattern

--- CAMEL/projectmatrix.cpp
+++ CAMEL/projectmatrix.cpp
@@ -1,8 +1,19 @@
 #include "projectmatrix.h"
 
+#define MAGIC_NUMBER            0xDEADFACE
+#define BIN_FILE_VERSION        1
+#define ERR_BAD_FILE_FORMAT     -1
+#define ERR_BAD_FILE_TOO_OLD    -2
+#define ERR_BAD_FILE_TOO_NEW    -3
+#define ERR_MTX_FMT_UNKNOWN     -4
+
 ProjectMatrix::ProjectMatrix(const QString M_Name, const quint16 M_Cols, const quint16 M_Rows, const quint32 M_ColorsDepth)       
 {
     MathMatrix ProjMatrix(M_Name,M_Cols,M_Rows,M_ColorsDepth);
+    Cols=M_Cols;
+    Rows=M_Rows;
+    ColorDepth=M_ColorsDepth;
+
     QVector<QVector<QRgb>> MatrixToRead(1);
 
     ProjMatrix.SetLine(1,0xFF000001) ;
@@ -14,8 +25,76 @@ ProjectMatrix::ProjectMatrix(const QString M_Name, const quint16 M_Cols, const q
 
     SequenceManager Sequencer();
     ConfigurationManager ConfMgr;
+
     ConfMgr.SaveToFile("Sequencefile.ini", M_Name,  M_Cols,  M_Rows,  M_ColorsDepth);
+
     SavePattern("Pattern3.bin", MatrixToRead) ;
+
+    MatrixToRead.clear();
+    LoadPattern("Pattern3.bin", MatrixToRead) ;
+}
+
+
+int ProjectMatrix::LoadPattern(QString Filename, QVector<QVector<QRgb> > &MatrixVector)
+{
+    QFile filein(Filename);
+    if (!filein.open(QIODevice::ReadOnly))
+            return -1;
+
+    QDataStream in(&filein);
+
+    // Read and check the header
+    quint32 magic;
+    in >> magic;
+    if (magic != MAGIC_NUMBER)
+        return ERR_BAD_FILE_FORMAT;
+
+    // Read the version
+    quint16 version;
+    in >> version;
+    if (version < BIN_FILE_VERSION)
+        return ERR_BAD_FILE_TOO_OLD;
+    if (version > BIN_FILE_VERSION)
+        return ERR_BAD_FILE_TOO_NEW;
+
+    // Retrieve the sequence size ( how much patterns)
+    quint32 SeqSize ;
+    in >> SeqSize ;
+qDebug() << SeqSize ;
+
+    //    MatrixFormat
+     quint16 SeqFileRows, SeqFileCols ;
+     quint32 SeqFileColors ;
+    in >> SeqFileRows;
+    in >> SeqFileCols;
+    in >> SeqFileColors;
+ qDebug()  << SeqFileRows << SeqFileCols << SeqFileColors ;
+    if ( SeqFileRows!=8 && SeqFileCols!=8 && SeqFileColors!=3 )     // Check the dims of the matrix in the seq file
+        return ERR_MTX_FMT_UNKNOWN ;        ///WARNING Matrix other than 8x8@3 are not handled
+
+    QVector<QVector<QRgb> > Temp_MatrixVector;
+
+    QRgb RGBValue = 0 ;
+
+    for(uint i = 0; i < SeqSize; ++i)
+    {
+        for(uint indRow = 0;indRow < SeqFileRows; ++indRow )
+        {
+            for(uint indCol = 0;indCol < SeqFileCols ;++indCol)
+            {
+                QVector<QRgb> inner_vector;
+                in >> RGBValue;
+
+                inner_vector.push_back(RGBValue);
+                Temp_MatrixVector.push_back(inner_vector);
+            }
+        }
+    }
+
+    for (auto it: Temp_MatrixVector)
+        qDebug() << it ;
+
+    return 0 ;
 }
 
 
@@ -26,6 +105,8 @@ void ProjectMatrix::ReadMatrix(MathMatrix ImgToRead, QVector<QVector<QRgb>> &Mat
     ImgToRead.CopyPatternToSequence( MatrixVector,CurrentVector_Index++  );
 }
 
+
+
 // Save the Current defined matrix as a pattern in a binary file
 
 void ProjectMatrix::SavePattern(QString Filename, QVector<QVector<QRgb>> &MatrixVector)
@@ -35,8 +116,13 @@ void ProjectMatrix::SavePattern(QString Filename, QVector<QVector<QRgb>> &Matrix
     {
         QDataStream out(&fileout);
         // Header
-        out << (quint32)0xDEADFACE;  // Magic Number
+        out << (quint32) MAGIC_NUMBER ;             // Magic Number
+        out << (quint16) BIN_FILE_VERSION;
         out << (quint16) MatrixVector.size() - 1;  // Tells how much patterns this sequence file have
+        out << (quint16) Rows;
+        out << (quint16) Cols;
+        out << (quint32) ColorDepth;
+
 
         // now save the Patterns Data from the 2D Vector
         for(int i = 0;i < MatrixVector.size();++i)
--- CAMEL/projectmatrix.h
+++ CAMEL/projectmatrix.h
@@ -11,14 +11,16 @@ class ProjectMatrix
 {
 private:
     MathMatrix ProjMatrix ;
-
-
+    quint16 Cols={};
+    quint16 Rows={};
+    quint32 ColorDepth={} ;
 public:
     static QVector<QVector<QRgb>> MatrixToRead;
     int CurVector_Index;
     ProjectMatrix(const QString M_Name, const quint16 M_Cols, const quint16 M_Rows, const quint32 M_ColorsDepth);
     void ReadMatrix(MathMatrix ImgToRead, QVector<QVector<QRgb> > &MatrixVector);
     void SavePattern(QString Filename, QVector<QVector<QRgb> > &MatrixVector);
+    int LoadPattern(QString Filename, QVector<QVector<QRgb> > &MatrixVector);
 };
 
 #endif // PROJECTMATRIX_H
